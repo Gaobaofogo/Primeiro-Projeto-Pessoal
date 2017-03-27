65b79c4a01132fc4ef60ed3c988befe79014aaef
Gabriel <gabrieldiniz54@gmail.com>
2017-03-27 11:35:41 -0300

    27/03 Usuario pode se cadastrar com email

---
+++ mysite/accounts/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from . import views
+
+app_name = "accounts"
+urlpatterns = [
+    url(r'^entrar/$', auth_views.login, {'template_name': 'accounts/login.html'}, name="login"),
+    url(r'^cadastro/$', views.register, name="register"),
+]
